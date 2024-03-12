a3bd3f4fbb531864ad260b17e3179774855d8645
simone-danzeri <s.danzeri@icloud.com>
2024-03-12 13:26:59 +0100

    feat: SNACK 1 - Complete

--- SNACK 1/js/script.js
+++ SNACK 1/js/script.js
@@ -4,4 +4,13 @@
 
 // Chiedo il numero all'utente
 const userNumber = parseInt(prompt('Ciao! Dimmi un numero'));
-console.log(userNumber);
+// console.log(userNumber);
+
+// Controllo se il numero è pari
+if (userNumber % 2 === 0) {
+    // Lo stampo così com'è
+    console.log(userNumber);
+} else { // Se invece è dispari
+    // Stampo il successivo
+    console.log(userNumber + 1);
+}
