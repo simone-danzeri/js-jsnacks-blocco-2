69b4ea9b553cc05db2adc747afb3396e33a6ff45
simone-danzeri <s.danzeri@icloud.com>
2024-03-12 13:46:41 +0100

    feat: SNACK 2 - Complete

--- SNACK 2/js/script.js
+++ SNACK 2/js/script.js
@@ -3,12 +3,26 @@
 
 // Chiedo all'utente due parole
 let userWord1 = prompt('Ciao! Scrivi una parola');
-console.log(userWord1);
+// console.log(userWord1);
 let userWord2 = prompt("Ora scrivine un'altra");
-console.log(userWord2);
+// console.log(userWord2);
 
 // Devo controllare la lunghezza di ciascuna parola
 let userWord1Length = userWord1.length;
-console.log(userWord1Length);
+// console.log(userWord1Length);
 let userWord2Length = userWord2.length;
-console.log(userWord2Length);
+// console.log(userWord2Length);
+
+// Stampo le due parole in ordine crescente di lunghezza
+// Devo confrontare le due parole
+if (userWord1Length > userWord2Length) {
+    // Se la parola numero 1 è più lunga --> Stampo per prima la parola numero 2 e poi la parola numero 1
+    console.log(userWord2);
+    console.log(userWord1);
+} else if (userWord1Length < userWord2Length) {
+    // Se la parola numero 2 è più lunga --> Stampo per prima la parola numero 1 e poi la parola numero 2
+    console.log(userWord1);
+    console.log(userWord2);
+} else {
+    console.log("Le due parole hanno lunghezza uguale quindi beccatele nell'ordine in cui le hai scritte: " + userWord1 + ' ' + userWord2);
+}
