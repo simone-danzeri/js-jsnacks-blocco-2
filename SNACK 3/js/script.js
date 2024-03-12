ee347ecb6e5ae37bc7b475debea676aa393a6e01
simone-danzeri <s.danzeri@icloud.com>
2024-03-12 13:52:36 +0100

    feat: SNACK 3 - add array with numbers

--- SNACK 3/js/script.js
+++ SNACK 3/js/script.js
@@ -1,3 +1,7 @@
 // Crea due tag div con due id diversi:
 // Un div avrà il testo colorato di rosso mentre l’altro di verde.
 // Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+// Mi creo un array di numeri
+ const numbersArray = [2, 5, 56, 23, 9, 22, 71, 65, 13, 1, 90];
+ console.log(numbersArray);
