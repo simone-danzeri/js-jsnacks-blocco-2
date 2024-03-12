64fc78e63c5c2f96f23c7963cd7d3be233bb2d60
simone-danzeri <s.danzeri@icloud.com>
2024-03-12 13:22:22 +0100

    feat: SNACK 1 - add userNumber

---
+++ SNACK 1/js/script.js
@@ -0,0 +1,7 @@
+// L'utente inserisce un numero nel prompt.
+// Se è pari stampa il numero.
+// Se è dispari stampa il numero successivo
+
+// Chiedo il numero all'utente
+const userNumber = parseInt(prompt('Ciao! Dimmi un numero'));
+console.log(userNumber);
